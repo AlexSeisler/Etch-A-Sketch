9a39fda825a256a966c604309758d136e2b06455
Alex Seisler <aseisler7045@stevenscollege.edu>
2024-09-27 19:39:51 -0400

    Finished Etch-A-Sketch(before opacity)

--- Etch.js
+++ Etch.js
@@ -1,5 +1,4 @@
-const gridContainer = document.getElementById('grid-container');
-let amount=16;
+
 
 function randColor()
 {
@@ -14,23 +13,39 @@ function randColor()
 
 function changeScale()
 {
-    for (let i = 0; i < amount*amount; i++) 
-        {
-            const div = document.createElement('div');
-            div.classList.add('grid-item');
-            
-            div.addEventListener('mouseover', function() {
-                div.style.backgroundColor = 'lightcoral'; // Change color on hover
-            });
-    
-            div.addEventListener('mouseout', function() {
-                div.style.backgroundColor = randColor(); // Revert color when hover ends
-            });
-            
-            gridContainer.appendChild(div);
+    var gridContainer = document.getElementById('grid-container');
+    let amount=16;
+    gridContainer.innerHTML = '';
+    const gridSize = parseInt(document.getElementById('gridSizeInput').value);
+    if(gridSize<101 &&gridSize>0)
+    {
+        const containerSize = gridSize * 20;
+        gridContainer.style.width = `${containerSize}px`;
+        gridContainer.style.height = `${containerSize}px`;
+
+        for (let i = 0; i < gridSize*gridSize; i++) 
+            {
+                const div = document.createElement('div');
+                div.classList.add('grid-item');
+                
+                div.addEventListener('mouseover', function() {
+                    div.style.backgroundColor = 'lightcoral'; // Change color on hover
+                });
+        
+                div.addEventListener('mouseout', function() {
+                    div.style.backgroundColor = randColor(); // Revert color when hover ends
+                });
+                
+                gridContainer.appendChild(div);
+        }
+    }
+    else
+    {
+        gridContainer.innerHTML = "Invalid Input try again(1-100)";
     }
 }
-
+var gridContainer = document.getElementById('grid-container');
+let amount=16;
 for (let i = 0; i < amount*amount; i++) 
     {
         const div = document.createElement('div');
